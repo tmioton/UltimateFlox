42c5245b438f3ef4b45a3dec67bd8b95a71a23d0
Tate Mioton <tatemioton@outlook.com>
2021-09-01 00:14:50 -0500

    Add Option to Disable 'f' after Outputs

--- colorconvert.py
+++ colorconvert.py
@@ -3,7 +3,7 @@
 from sys import argv, stderr
 
 
-def convert(code) -> int:
+def convert(code, floating=False) -> int:
     if len(code) != 6:
         print("Proper hex color code expected.", file=stderr)
         return 1
@@ -14,9 +14,9 @@ def convert(code) -> int:
         try:
             values.append(round(int(individual, base=16) / 0xFF, 5))
         except ValueError:
-            print(f"Invalid hex code:\n{' '.join(codes)}\n{(' ' * (i * 4 + i))}^^^^", file=stderr)
+            print(f"Invalid hex code:\n{' '.join(codes)}\n{(' ' * (i * 5))}^^^^", file=stderr)
             return 1
-    print(f", ".join(map('{0}f'.format, values)))
+    print(f", ".join(map(('{0}f' if floating else '{0}').format, values)))
     return 0
 
 
@@ -25,4 +25,11 @@ if __name__ == '__main__':
         print("Hex color code expected as argument.", file=stderr)
         exit(1)
     else:
-        exit(convert(argv[1]))
+        floating = False
+        if argv[1] == "-f":
+            floating = True
+            argv.pop(1)
+        elif argv[1] == "-d":
+            floating = False
+            argv.pop(1)
+        exit(convert(argv[1], floating))
