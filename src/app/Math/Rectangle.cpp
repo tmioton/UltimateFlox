cb1abbed3ad75aacb6c13ffd6d2074b3225c8c73
Tate Mioton <tatemioton@outlook.com>
2022-11-26 15:43:17 -0600

    Add Simple Center-Defined Rectangle Class

---
+++ src/app/Math/Rectangle.cpp
@@ -0,0 +1,31 @@
+#include "Rectangle.hpp"
+
+Rectangle::Rectangle(Vector c, Vector s) : center(c), size(s) {}
+
+Rectangle::Rectangle(Vector s) : center(0.0f, 0.0f), size(s) {}
+
+bool Rectangle::contains(Vector p) const {
+    return (
+        (p.x >= center.x - size.x && p.x <= center.x + size.x)
+        && (p.y >= center.y - size.y && p.y <= center.y + size.y)
+    );
+}
+
+bool Rectangle::intersects(Rectangle &other) const {
+    return !(
+        other.center.x + other.size.x < center.x - size.x ||
+        other.center.x - other.size.x > center.x + size.x ||
+        other.center.y + other.size.y < center.y - size.y ||
+        other.center.y - other.size.y > center.y + size.y
+    );
+}
+
+int Rectangle::quadrant(Vector p) const {
+    bool x {p.x >= center.x};
+    bool y {p.y >= center.y};
+    return ((!y << 1) + (((!x) & y) | (x & (!y)))) & 0b11;
+}
+
+Rectangle Rectangle::operator*(float val) const {
+    return {center, size * val};
+}
---
+++ src/app/Math/Rectangle.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class Rectangle {
+public:
+    Rectangle() = default;
+    Rectangle(Vector center, Vector size);
+    explicit Rectangle(Vector size);
+
+    Rectangle(Rectangle const&) = default;
+    Rectangle(Rectangle&&) = default;
+    Rectangle& operator=(Rectangle const&) = default;
+    Rectangle& operator=(Rectangle&&) = default;
+
+    Rectangle operator*(float) const;
+
+    [[nodiscard]] bool contains(Vector point) const;
+    [[nodiscard]] bool intersects(Rectangle&) const;
+    [[nodiscard]] int quadrant(Vector point) const;
+
+    Vector center{0, 0};
+    Vector size{0, 0};
+};
