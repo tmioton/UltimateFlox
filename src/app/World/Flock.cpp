55a1292449d51d3d47df546bd21df2560592f77b
Tate Mioton <tatemioton@outlook.com>
2022-11-26 15:46:04 -0600

    Place Boids Using Spiral Pattern

--- src/app/World/Flock.cpp
+++ src/app/World/Flock.cpp
@@ -5,13 +5,17 @@
 Flock::Flock(size_t flock_size) :
     m_count(flock_size), m_flock(flock_size) {
     // Set up boid starting locations
-    const float tauOverSize = glm::two_pi<float>() / static_cast<float>(m_count);
     Boid *writable = m_flock.write();
+
+    float d = 7.5f;
+    float angle = 0.0f;
     for (ptrdiff_t i = 0; i < m_count; i++) {
         Boid &boid = writable[i];
-        auto angle = static_cast<float>(i) * tauOverSize;
-        Vector offsets{cosf(angle), sinf(angle)};
-        boid.position = 50.0f * offsets;
+        //auto angle = static_cast<float>(i) * tauOverSize;
+        float radius = glm::sqrt(static_cast<float>(i + 1));
+        angle += glm::asin(1.0f / radius);
+        Vector offsets{glm::cos(angle) * radius * d, glm::sin(angle) * radius * d};
+        boid.position = offsets;
         boid.velocity = magnitude(10.0f * offsets + angle, Boid::maxSpeed);
     }
 
