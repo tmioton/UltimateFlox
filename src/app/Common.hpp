c96c6eea58bf1a6a88114f5063d9ba1979da8675
Tate Mioton <tatemioton@outlook.com>
2022-12-05 09:42:39 -0600

    Overload ostream Operator for Vector Types

--- src/app/Common.hpp
+++ src/app/Common.hpp
@@ -3,3 +3,14 @@
 using Vector = glm::vec2;
 using Color = glm::vec3;
 using Projection = glm::mat4;
+
+template<int length>
+std::ostream& operator<<(std::ostream& os, glm::vec<length, float, glm::defaultp> vec) {
+    os << '(';
+    for(int i = 0; i < length; ++i) {
+        if (i) { os << ", "; }
+        os << vec[i];
+    }
+    os << ')';
+    return os;
+}
