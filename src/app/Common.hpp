65026b9919ba821177d11a0c054ec276eb7b1044
Tate Mioton <tatemioton@outlook.com>
2022-12-30 08:42:12 -0600

    Include Full Template Parameters in Vector ostream Overload

--- src/app/Common.hpp
+++ src/app/Common.hpp
@@ -4,8 +4,8 @@ using Vector = glm::vec2;
 using Color = glm::vec3;
 using Projection = glm::mat4;
 
-template<int length>
-std::ostream& operator<<(std::ostream& os, glm::vec<length, float, glm::defaultp> vec) {
+template<int length, typename T, glm::qualifier Q>
+std::ostream& operator<<(std::ostream& os, glm::vec<length, T, Q> vec) {
     os << '(';
     for(int i = 0; i < length; ++i) {
         if (i) { os << ", "; }
