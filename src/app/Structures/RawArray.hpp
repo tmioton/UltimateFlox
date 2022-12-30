9be2d09159a5b094e08f3720a6fb218aadf2f40f
Tate Mioton <tatemioton@outlook.com>
2022-12-30 08:33:29 -0600

    Rework RawArray Structure to Return iterator Type

--- src/app/Structures/RawArray.hpp
+++ src/app/Structures/RawArray.hpp
@@ -1,18 +1,48 @@
 #pragma once
+
 #include "pch.hpp"
 
 template<typename T>
 class RawArray {
-    T* m_data;
+    struct iterator {
+        using iterator_category = std::contiguous_iterator_tag;
+        using value_type = T;
+        using difference_type = ptrdiff_t;
+        using pointer = T*;
+        using reference = T&;
+
+        explicit iterator(pointer _ptr) : ptr(_ptr) {}
+
+        iterator &operator=(iterator const&) noexcept = default;
+        _NODISCARD reference operator*() const noexcept { return *ptr; }
+        _NODISCARD pointer operator->() const noexcept { return ptr; }
+        iterator &operator++() noexcept { ++ptr; return *this; }
+        iterator operator++(int) noexcept { iterator retval = *this; ++(*this); return retval; } // Gets a warning, but this is exactly the msvc code for vector::operator++(int)
+        iterator &operator--() noexcept { --ptr; return *this; }
+        iterator operator--(int) noexcept { iterator retval = *this; --(*this); return retval; }  // Gets a warning, but this is exactly the msvc code for vector::operator--(int)
+        iterator &operator+=(difference_type _off) noexcept { ptr += _off; return *this; }
+        _NODISCARD iterator operator+(difference_type _off) const noexcept { iterator retval = *this; retval += _off; return retval; }
+        iterator &operator-=(difference_type _off) noexcept { ptr += -_off; return *this; }
+        _NODISCARD iterator operator-(difference_type _off) const noexcept { iterator retval = *this; retval -= _off; return retval; }
+        _NODISCARD difference_type operator-(iterator const& _rhs) const noexcept { return ptr - _rhs.ptr; }
+        _NODISCARD reference operator[](const difference_type _off) const noexcept { return *(*this + _off); }
+        _NODISCARD bool operator==(iterator const& _rhs) const noexcept { return ptr == _rhs.ptr; }
+        _NODISCARD std::strong_ordering operator<=>(iterator const& _rhs) { return std::addressof(*ptr) <=> std::addressof(*_rhs.ptr); }
+
+    private:
+        pointer ptr;
+    };
+
+    T *m_data;
     std::size_t m_size;
 public:
-    RawArray(T* data, std::size_t size) : m_data(data), m_size(size) {}
+    RawArray(T *data, std::size_t size) : m_data(data), m_size(size) {}
 
-    T* begin() {
-        return m_data;
+    iterator begin() {
+        return iterator(m_data);
     }
 
-    T* end() {
-        return m_data + m_size;
+    iterator end() {
+        return iterator(m_data + m_size);
     }
 };
