088f2f5408f08af4ab290dbfe657ed7cc3426ccb
Tate Mioton <tatemioton@outlook.com>
2023-01-11 14:17:13 -0600

    Reorder PCH Includes

--- src/app/pch.hpp
+++ src/app/pch.hpp
@@ -1,14 +1,6 @@
 #pragma once
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <lwvl/lwvl.hpp>
-#include <glm/vec2.hpp>
-#include <glm/gtc/constants.hpp>
-#include <glm/gtx/fast_square_root.hpp>
-#include <glm/gtx/norm.hpp>
-#include <lua/lua.hpp>
-
+// STL
 #include <iostream>
 #include <optional>
 #include <functional>
@@ -20,4 +12,15 @@
 #include <thread>
 #include <chrono>
 
+// EXTERNAL
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <lwvl/lwvl.hpp>
+#include <glm/vec2.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/gtx/fast_square_root.hpp>
+#include <glm/gtx/norm.hpp>
+#include <lua/lua.hpp>
+
+// APPLICATION
 #include "Common.hpp"
