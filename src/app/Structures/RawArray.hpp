85be5484ffed9e8714a1257b821a45aeafa41fdf
Tate Mioton <tatemioton@outlook.com>
2023-01-22 03:38:50 -0600

    Use [[nodiscard]] on RawArray::iterator

--- src/app/Structures/RawArray.hpp
+++ src/app/Structures/RawArray.hpp
@@ -14,20 +14,20 @@ class RawArray {
         explicit iterator(pointer _ptr) : ptr(_ptr) {}
 
         iterator &operator=(iterator const&) noexcept = default;
-        _NODISCARD reference operator*() const noexcept { return *ptr; }
-        _NODISCARD pointer operator->() const noexcept { return ptr; }
+        [[nodiscard]] reference operator*() const noexcept { return *ptr; }
+        [[nodiscard]] pointer operator->() const noexcept { return ptr; }
         iterator &operator++() noexcept { ++ptr; return *this; }
         iterator operator++(int) noexcept { iterator retval = *this; ++(*this); return retval; } // Gets a warning, but this is exactly the msvc code for vector::operator++(int)
         iterator &operator--() noexcept { --ptr; return *this; }
         iterator operator--(int) noexcept { iterator retval = *this; --(*this); return retval; }  // Gets a warning, but this is exactly the msvc code for vector::operator--(int)
         iterator &operator+=(difference_type _off) noexcept { ptr += _off; return *this; }
-        _NODISCARD iterator operator+(difference_type _off) const noexcept { iterator retval = *this; retval += _off; return retval; }
+        [[nodiscard]] iterator operator+(difference_type _off) const noexcept { iterator retval = *this; retval += _off; return retval; }
         iterator &operator-=(difference_type _off) noexcept { ptr += -_off; return *this; }
-        _NODISCARD iterator operator-(difference_type _off) const noexcept { iterator retval = *this; retval -= _off; return retval; }
-        _NODISCARD difference_type operator-(iterator const& _rhs) const noexcept { return ptr - _rhs.ptr; }
-        _NODISCARD reference operator[](const difference_type _off) const noexcept { return *(*this + _off); }
-        _NODISCARD bool operator==(iterator const& _rhs) const noexcept { return ptr == _rhs.ptr; }
-        _NODISCARD std::strong_ordering operator<=>(iterator const& _rhs) { return std::addressof(*ptr) <=> std::addressof(*_rhs.ptr); }
+        [[nodiscard]] iterator operator-(difference_type _off) const noexcept { iterator retval = *this; retval -= _off; return retval; }
+        [[nodiscard]] difference_type operator-(iterator const& _rhs) const noexcept { return ptr - _rhs.ptr; }
+        [[nodiscard]] reference operator[](const difference_type _off) const noexcept { return *(*this + _off); }
+        [[nodiscard]] bool operator==(iterator const& _rhs) const noexcept { return ptr == _rhs.ptr; }
+        [[nodiscard]] std::strong_ordering operator<=>(iterator const& _rhs) { return std::addressof(*ptr) <=> std::addressof(*_rhs.ptr); }
 
     private:
         pointer ptr;
